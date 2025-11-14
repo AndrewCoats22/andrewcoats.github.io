c5db61350ada75feb4b3b2dc2c5b94073b5b8068
AndrewCoats22 <coolcoats@hotmail.com>
2025-11-13 22:26:39 -0800

    Modified implementation

--- assignment_8/app.js
+++ assignment_8/app.js
@@ -22,27 +22,22 @@ app.get('/math/circle/:r', function (req, res) {
   });
 });
 
-app.get('/hello/name/:first?/:last?', function (req, res) {
+app.get('/hello/name/:first/:last', function (req, res) {
   let first = req.params.first;
   let last = req.params.last;
   
-  let missing = [];
-  if (!first) {
-    missing.push('first');
-  }
-  if (!last) {
-    missing.push('last');
-  }
-  
-  // If any parameters are missing, return error
-  if (missing.length > 0) {
-    res.type('text');
-    res.status(400).send('Missing Required GET parameters: ' + missing.join(', '));
-    return;
-  }
-
   res.type('text');
   res.send('Hello ' + first + ' ' + last);
+});
+
+app.get('/hello/name/:first', function (req, res) {
+  res.type('text');
+  res.status(400).send('Missing Required GET parameters: last');
+});
+
+app.get('/hello/name', function (req, res) {
+  res.type('text');
+  res.status(400).send('Missing Required GET parameters: first, last');
 });
 
 
