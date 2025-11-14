7d3b22442f18f83c7287f85cf87e99eb7d8a6995
AndrewCoats22 <coolcoats@hotmail.com>
2025-11-13 18:40:13 -0800

    small changes

--- assignment_5/encrypt-it.js
+++ assignment_5/encrypt-it.js
@@ -1,6 +1,4 @@
-/*
- * Starter file 
- */
+
 (function() {
   "use strict";
 
@@ -12,20 +10,16 @@
   window.addEventListener("load", init);
 
   /**
-   * TODO: Write a function comment using JSDoc.
+   * Initializes the page by setting up event handlers for the buttons.
+   * Logs a message to the console when the window loads.
    */
   function init() {
-    // Note: In this function, we usually want to set up our event handlers
-    // for UI elements on the page.
     console.log("Window loaded!");
     
     document.getElementById("encrypt-it").addEventListener("click", handleClick);
     document.getElementById("reset").addEventListener("click", handleReset);
   }
 
-  // Add any other functions in this area (you should not implement your
-  // entire program in the init function, for similar reasons that
-  // you shouldn't write an entire Java program in the main method).
   function handleClick() {
     console.log("Button clicked!");
     
