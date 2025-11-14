02d26ebd14b6bdcccb0f9fdae5ee9b66bba59eba
AndrewCoats22 <coolcoats@hotmail.com>
2025-11-13 21:37:41 -0800

    final version of ajaxpets.js

--- assignment_7/ajaxpets.js
+++ assignment_7/ajaxpets.js
@@ -3,28 +3,65 @@
  * kitties will be populated on the page after the user selects their desired
  * pet type.
  */
+
 "use strict";
 (function() {
+
+  const AJAX_PETS_URL = "https://courses.cs.washington.edu/courses/cse154/webservices/pets/ajaxpets.php";
 
   window.addEventListener("load", init);
 
   /**
-   * TODO: What do we need to initialize?
+   * Sets up the radio buttons for the Ajax Pet Gallery for a user to select what type of
+   * cute pet photos they want to see (they should of course, always pick puppies)
    */
   function init() {
-    // TODO
+    let radios = qsa("input[name='animal']");
+    for (let i = 0; i < radios.length; i++) {
+      radios[i].addEventListener("change", makeRequest);
+    }
   }
 
   /**
-   * TODO: Fetch data from the CSE 154 ajax pets api!
+   * Updates the photos displayed on the page based on the current pet selection, fetching the
+   * photos from the AJAX Pets API.
    */
   function makeRequest() {
-    // TODO
+    let animal = this.value;
+
+    fetch(AJAX_PETS_URL + "?animal=" + animal)
+      .then(checkStatus)
+      .then(resp => resp.text())
+      .then(displayPictures)
+      .catch(console.error);
   }
 
   /**
-   * TODO: Implement any other functions you need
+   * Populates the pictures section of the page with the result images,
+   * replacing any previous photos.
+   * @param {string} response - text of all of pet image sources
    */
+  function displayPictures(response) {
+    response = response.trim().split("\n");
+    clearPictures();
+    for (let i = 0; i < response.length; i++) {
+      let imgPath = response[i];
+      let img = document.createElement("img");
+      img.src = imgPath;
+      img.alt = "adorable friend";
+      id("pictures").appendChild(img);
+    }
+  }
+
+  /**
+   * Clears any images in the photo gallery container.
+   */
+  function clearPictures() {
+    let imgs = qsa("#pictures img");
+    for (let i = 0; i < imgs.length; i++) {
+      imgs[i].parentNode.removeChild(imgs[i]);
+    }
+  }
 
   /* ------------------------------ Helper Functions  ------------------------------ */
 
@@ -49,15 +86,6 @@
    */
   function id(id) {
     return document.getElementById(id);
-  }
-
-  /**
-   * Returns the first element that matches the given CSS selector.
-   * @param {string} query - CSS query selector.
-   * @returns {object[]} array of DOM objects matching the query.
-   */
-  function qs(query) {
-    return document.querySelector(query);
   }
 
   /**
